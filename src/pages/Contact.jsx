f14fb44dfe01ce044747a2bc70964e11d605197e
Hema4506 <hvsnagalakshmi@gmail.com>
2025-07-04 22:30:35 +0530

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,8 +2,8 @@ const Contact = () => {
   return (
     <div className="page contact">
       <h1>Contact Us</h1>
-      <p>Email: kanoj@example.com</p>
-      <p>Phone: 8555017044</p>
+      <p>Email: hema123@gmail.com</p>
+      <p>Phone: 9876543210</p>
     </div>
   )
 }
